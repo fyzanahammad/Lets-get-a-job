001ed937f3d1159c449a2bb5bf273abc8a961d41
Fyzan Ahammad <fyzanahammad@gmail.com>
2024-07-19 18:28:12 +0530

    cleaning the repo

---
+++ resume_builder.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def resume_builder_page():
+    st.title('Resume Builder')
+    st.write('This page will be used for building resumes.')
+
+    # Add your resume building logic here
+    # Example: Form inputs, document generation, etc.
