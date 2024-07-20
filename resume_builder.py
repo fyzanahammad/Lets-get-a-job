f3a3813d2351d729851c24e95b4d67abfee99c9f
Fyzan Ahammad <fyzanahammad@gmail.com>
2024-07-20 10:20:50 +0530

    updated files with gemini api integration

--- resume_builder.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def resume_builder_page():
-    st.title('Resume Builder')
-    st.write('This page will be used for building resumes.')
-
-    # Add your resume building logic here
-    # Example: Form inputs, document generation, etc.
